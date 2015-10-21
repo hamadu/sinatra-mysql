cbfa8dc219a508873d796d672eb248e06b2b4f2c
hama_du <dercll1020@gmail.com>
2015-10-21 22:09:01 +0900

    add production_local mode

--- app.rb
+++ app.rb
@@ -4,37 +4,39 @@ require 'mysql2'
 require 'json'
 require 'yaml'
 
-connection_setting = YAML.load(ERB.new(File.read('database.yml')).result)
-ActiveRecord::Base.establish_connection connection_setting[ENV['RACK_ENV'] || 'development']
+class MyApp < Sinatra::Base
+  connection_setting = YAML.load(ERB.new(File.read('database.yml')).result)
+  ActiveRecord::Base.establish_connection connection_setting['development']
 
-class User < ActiveRecord::Base
-end
+  class User < ActiveRecord::Base
+  end
 
-get '/' do
-  'やっはろー'
-end
+  get '/' do
+    'やっはろー'
+  end
 
-get '/generate' do
-  name = [*('a'..'z')].sample(16).join
-  User.create(name: name, score: 0)
-  name
-end
+  get '/generate' do
+    name = [*('a'..'z')].sample(16).join
+    User.create(name: name, score: 0)
+    name
+  end
 
-get '/reset' do
-  User.delete_all
-  'deleted'
-end
+  get '/reset' do
+    User.delete_all
+    'deleted'
+  end
 
-get '/ranking' do
-  User.order(score: :desc).limit(50).to_json
-end
+  get '/ranking' do
+    User.order(score: :desc).limit(50).to_json
+  end
 
-get '/request' do
-  name = params['name']
-  user = User.find_by(name: name)
-  return '404' unless user
+  get '/request' do
+    name = params['name']
+    user = User.find_by(name: name)
+    return '404' unless user
 
-  user.score += rand(100)
-  user.save!
-  'done'
+    user.score += rand(100)
+    user.save!
+    'done'
+  end
 end
